033861953bf29b36aec788ebd0c8cf4b96b0b750
Stefan Vasic <stefan_vasic@outlook.com>
2025-02-18 01:22:02 +0100

    Add button order property

--- lib/src/show_spinner_time_picker_dialog.dart
+++ lib/src/show_spinner_time_picker_dialog.dart
@@ -5,45 +5,53 @@ import 'package:flutter/material.dart';
 // Import custom widget used in the dialog
 import 'spinner_time_picker_widget.dart';
 
+enum ButtonOrder {
+  saveCancel,
+  cancelSave,
+}
+
 // Function to show a dialog with a spinner-based time picker
-Future<TimeOfDay?> showSpinnerTimePicker(
-  BuildContext context, {
-  // Optional parameters for customizing the appearance and behavior of the dialog
-  String? title,
-  Color? backgroundColor,
-  Color? foregroundColor,
-  TextStyle? titleStyle,
-  ButtonStyle? buttonStyle,
-  TextStyle? buttonTextStyle,
-  bool barrierDismissible = true,
-  TimeOfDay? initTime,
-  bool? is24HourFormat,
-  double? height,
-  double? width,
-  double? spinnerHeight,
-  double? spinnerWidth,
-  double? elementsSpace,
-  double? digitHeight,
-  Color? spinnerBgColor,
-  TextStyle? selectedTextStyle,
-  TextStyle? nonSelectedTextStyle,
-  EdgeInsets? contentPadding,
-  String? cancelButtonLabel,
-  String? okButtonLabel,
-}) async {
+Future<TimeOfDay?> showSpinnerTimePicker(BuildContext context,
+    {
+    // Optional parameters for customizing the appearance and behavior of the dialog
+    String? title,
+    Color? backgroundColor,
+    Color? foregroundColor,
+    TextStyle? titleStyle,
+    ButtonStyle? buttonStyle,
+    TextStyle? buttonTextStyle,
+    bool barrierDismissible = true,
+    TimeOfDay? initTime,
+    bool? is24HourFormat,
+    double? height,
+    double? width,
+    double? spinnerHeight,
+    double? spinnerWidth,
+    double? elementsSpace,
+    double? digitHeight,
+    Color? spinnerBgColor,
+    TextStyle? selectedTextStyle,
+    TextStyle? nonSelectedTextStyle,
+    EdgeInsets? contentPadding,
+    String? cancelButtonLabel,
+    String? okButtonLabel,
+    ButtonOrder? buttonOrder = ButtonOrder.saveCancel}) async {
   // Get the color scheme and screen size from the current theme
   final colorScheme = Theme.of(context).colorScheme;
   final size = MediaQuery.of(context).size;
-  final Brightness currentBrightness = MediaQuery.of(context).platformBrightness;
+  final Brightness currentBrightness =
+      MediaQuery.of(context).platformBrightness;
 
   // Check the current brightness mode
   final bool isDarkMode = currentBrightness == Brightness.dark;
 
   // Set default values for various optional parameters
-  final _foregroundColor = foregroundColor ?? colorScheme.onBackground.withAlpha(200);
+  final _foregroundColor =
+      foregroundColor ?? colorScheme.onBackground.withAlpha(200);
   final _backgroundColor = backgroundColor ?? colorScheme.background;
   final _title = title ?? "Select a Time";
-  final _titleStyle = titleStyle ?? TextStyle(fontSize: 18, color: _foregroundColor);
+  final _titleStyle =
+      titleStyle ?? TextStyle(fontSize: 18, color: _foregroundColor);
   final _is24HourFormat = is24HourFormat ?? true;
   final _height = height ?? 0.25 * size.height;
   final _width = width ?? 0.8 * size.width;
@@ -53,7 +61,8 @@ Future<TimeOfDay?> showSpinnerTimePicker(
   final _digitHeight = digitHeight ?? 0.35 * _spinnerHeight;
 
   // Set spinner background color based on dark mode status
-  final _spinnerBgColor = spinnerBgColor ?? (isDarkMode ? colorScheme.primary : colorScheme.primaryContainer);
+  final _spinnerBgColor = spinnerBgColor ??
+      (isDarkMode ? colorScheme.primary : colorScheme.primaryContainer);
 
   // Set default text styles for selected and non-selected time elements
   final _selectedTextStyle = selectedTextStyle ??
@@ -65,7 +74,9 @@ Future<TimeOfDay?> showSpinnerTimePicker(
   final _nonSelectedTextStyle = nonSelectedTextStyle ??
       TextStyle(
         fontSize: 30,
-        color: isDarkMode ? colorScheme.primaryContainer.withAlpha(200) : colorScheme.primary.withAlpha(150),
+        color: isDarkMode
+            ? colorScheme.primaryContainer.withAlpha(200)
+            : colorScheme.primary.withAlpha(150),
       );
 
   // Set default text style for buttons
@@ -84,24 +95,27 @@ Future<TimeOfDay?> showSpinnerTimePicker(
   String pressedButton = "Cancel";
 
   // Create the Cancel and Done buttons with their respective actions
-  final actionsButtons = <Widget>[
-    TextButton(
-      style: buttonStyle,
-      onPressed: () {
-        pressedButton = "Cancel";
-        Navigator.of(context).pop();
-      },
-      child: Text(_cancelButtonLabel, style: _buttonTextStyle),
-    ),
-    TextButton(
-      style: buttonStyle,
-      onPressed: () {
-        pressedButton = "Done";
-        Navigator.of(context).pop();
-      },
-      child: Text(_okButtonLabel, style: _buttonTextStyle),
-    ),
-  ];
+  final saveButton = TextButton(
+    style: buttonStyle,
+    onPressed: () {
+      pressedButton = "Done";
+      Navigator.of(context).pop();
+    },
+    child: Text(_okButtonLabel, style: _buttonTextStyle),
+  );
+
+  final cancelButton = TextButton(
+    style: buttonStyle,
+    onPressed: () {
+      pressedButton = "Cancel";
+      Navigator.of(context).pop();
+    },
+    child: Text(_cancelButtonLabel, style: _buttonTextStyle),
+  );
+
+  final actionsButtons = buttonOrder == ButtonOrder.saveCancel
+      ? <Widget>[saveButton, cancelButton]
+      : <Widget>[cancelButton, saveButton];
 
   // Show the dialog and get the selected time when the dialog is dismissed
   await showDialog<void>(
@@ -109,7 +123,8 @@ Future<TimeOfDay?> showSpinnerTimePicker(
     barrierDismissible: barrierDismissible,
     builder: (context) {
       return Theme(
-        data: Theme.of(context).copyWith(dialogBackgroundColor: _backgroundColor),
+        data:
+            Theme.of(context).copyWith(dialogBackgroundColor: _backgroundColor),
         child: AlertDialog(
           contentPadding: contentPadding,
           title: Center(child: Text(_title, style: _titleStyle)),
